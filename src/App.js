71c90b23029ff17b6e3a0dda37fce610b850561e
anandhelloworld <anandmohanbabu1910@gmail.com>
2022-08-16 18:10:12 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Box from "./components/box";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Maincontainer from "./components/Maincontainer";
 
-function App() {
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleOrder = (e) => {
+    // console.log(e.target)
+    // console.log(e.target.innerHTML)
+    if (e.target.innerText == "Add to cart") {
+      console.log("breargk");
+      console.log(e.target);
+      console.log(e.target.innerText);
+      console.log(e.target.innerHTML);
+
+      setCounter((current) => current + 1);
+      e.target.innerText = "Remove from cart";
+
+      console.log("breegak");
+      console.log(e.target);
+      console.log(e.target.innerText);
+      console.log(e.target.innerHTML);
+    } else {
+      console.log("bretheak");
+      console.log(e.target);
+      console.log(e.target.innerText);
+      console.log(e.target.innerHTML);
+
+      setCounter((current) => current - 1);
+      e.target.innerText = "Add to cart";
+
+      console.log("breakr");
+      console.log(e.target);
+      console.log(e.target.innerText);
+      console.log(e.target.innerHTML);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header order={counter} />
+      <Box />
+      <Maincontainer click={handleOrder} />
+      <Footer />
     </div>
   );
 }
-
-export default App;
